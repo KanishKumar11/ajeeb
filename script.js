9c0d07f6ffa1c7f513b86b638e175f1fd8e30427
Kanish Kumar <71805719+KeenKanish@users.noreply.github.com>
2021-10-17 08:43:26 +0000

    h

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 function readOut(message){
   const speech = new SpeechSynthesisUtterance()
   speech.text = message;
-  speech.voice = allVoices[15]
+  speech.voice = allVoices[15];
   speech.volume = 1;
   window.speechSynthesis.speak(speech);
   console.log("speaking");
 }
 
 window.addEventListener('load', (event) => {
-  readOut("नमस्ते")
+  readOut("नमस्ते / Hello")
 });
 // audio input from user
 const texts = document.querySelector('.texts');
